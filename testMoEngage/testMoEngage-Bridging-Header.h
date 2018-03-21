18f3cce183631ef8b55f0777aec7ceb135afc682
Csipler Zoltan <z.csipler@mito.hu>
2018-03-21 12:44:14 +0100

    First commit

---
+++ testMoEngage/testMoEngage-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  testMoEngage-Bridging-Header.h
+//  testMoEngage
+//
+//  Created by Csipler Zoltan on 2018. 03. 21..
+//  Copyright © 2018. Mito Europe Kft. All rights reserved.
+//
+
+#ifndef testMoEngage_Bridging_Header_h
+#define testMoEngage_Bridging_Header_h
+
+#import "MoEngage.h"
+#import "MOInbox.h"
+#import "MOEHelperConstants.h"
+#import "MOGeofenceHandler.h"
+
+#endif /* testMoEngage_Bridging_Header_h */
